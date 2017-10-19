10baef2646a61ec7910e44f4e4a97d90efc7bd47
laffingragons <akshaypatil,vit@gmail.com>
2017-10-19 11:03:56 +0530

    language input problem

--- js/app.js
+++ js/app.js
@@ -1,49 +1,51 @@
- // main document ready function to check if dom is loaded fully or no
+ // main document ready function to check if dom is loaded fully or not
+  $( document ).ready(function() {
 
         
     // var myFacebookToken="";
          
-       function myFunction(){ 
+       $("#info").click(function(){
     var myFacebookToken = $("#token").val();
            console.log(myFacebookToken);
-      
-       // function getFacebookInfo(){
-        $.ajax('https://graph.facebook.com/me?fields=link,email,hometown&access_token='+myFacebookToken,{
+    
+     
+        //function getFacebookInfo(){
+        $.ajax('https://graph.facebook.com/me?fields=picture.width(250).height(250),id,name,first_name,last_name,birthday,about,hometown,languages,gender,education,work,relationship_status,quotes,family,website,email,cover&access_token='+myFacebookToken,{
 
                 success : function(response){
                     console.log(response);
                     console.log(typeof(response));
-                    $("#myFirstName").text(response.email);
+                    $("#myEmail").text(response.email);
                     $("#myProfileId").html('<a target="blank" href="https://facebook.com/'+response.id+'">https://facebook.com/'+response.id+'</a>');
                     $("#myHomeTown").text(response.hometown.name);
-                    //$(".myProfilePic").attr("src",""+response.picture.data.url+"");
-  //                  $("#myName").html(response.name);
-    //                $("#myFirstName").html(response.first_name);
-      ///              $("#myLastName").html(response.last_name);
-		//			$("#myBirthday").html(response.birthday);
-		//			$("#myGender").html(response.gender);
+                    $(".myProfilePic").attr("src",""+response.picture.data.url+"");
+                    $("#myName").html(response.name);
+                    $("#myFirstName").html(response.first_name);
+                    $("#myLastName").html(response.last_name);
+					$("#myBirthday").html(response.birthday);
+					$("#myGender").html(response.gender);
+                    $("#myLanguage").html(response.languages);
                     //for language 
-     //               if(response.languages != undefined && response.languages !=null){
-       //                $("#myLanguage").html(response.languages); 
-         //           }
-           //         else{
-             //           $("#myLanguage").html("empty");
-               //     }
+                    
+                    
                     
                 }
-            }//end argument list 
+            //end argument list 
 
 
-
+        }
         );// end ajax call 
 
 
     }// end get facebook info
-
+);
+      
+  });
+           
    //$("#facebookBtn").on('click',getFacebookInfo);
       
     
 
 
 
- 
+  
