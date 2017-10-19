786c99c72edaab4df04fd1caea5949a417aa26b1
laffingragons <akshaypatil,vit@gmail.com>
2017-10-20 01:21:01 +0530

    after this trying to add flip card

--- js/app1.js
+++ js/app1.js
@@ -19,51 +19,14 @@
                     $("#myProfileId").html('<a target="blank" href="https://facebook.com/'+response.id+'">https://facebook.com/'+response.id+'</a>');
                     $("#myHomeTown").text(response.hometown.name);
                     $(".myProfilePic").attr("src",""+response.picture.data.url+"");
-                    $(".myCoverPic").attr("src",""+response.cover.source+"");
                     $("#myName").html(response.name);
                     $("#myFirstName").html(response.first_name);
                     $("#myLastName").html(response.last_name);
 					$("#myBirthday").html(response.birthday);
 					$("#myGender").html(response.gender);
-                    $("#myAbout").html(response.about);
-                    $("#myWebsite").val(response.website);
-                    
+                    $("#myLanguage").html(response.languages.data.name);
                     //for language 
-                    var languages = response.languages ;
-                    var myLanguage= $.map(languages,function(index){
-					return index.name;
-				    });
-  	                $("#myLanguage").text(myLanguage);
                     
-                    // for Education
-                     var workData = response.work ;       
-					var workSections ="" ;
-					 var getWork = $.map(workData,function(index){    
-					 
-					 		for(var names in index) {                        // For getting Work details    
-					 
-					 if (names =="position") {
-					
-						workSections += (index.position.name+" - "+index.employer.name+ " | ");
-					}
-					}
-					$("#myWork").attr("value",""+workSections+""); 
-					
-					});  
-
-					 var eduData = response.education ;
-					 var getCollege;
-					 $.each(eduData,function(i,value){
-					 	 if(value.type == "College"){
-                            getCollege = value.school.name;							//  For getting School details    
-                            $("#myCollege").attr("value"," "+getCollege+"");
-                        } 
-                        else if (value.type == "High School"){
-                        	 getCollege = value.school.name;
-                        	 $("#myHighschool").attr("value"," "+getCollege+"");
-                        	}
-					 	}
-					 );
                     
                     
                 }
